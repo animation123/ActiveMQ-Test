fb82768d27adaa911112beb12557e4a32898debd
animation <763678968@qq.com>
2019-10-15 21:02:46 +0800

    SpringBoot整合ActiveMQ 定时投送

--- boot_mq_produce/src/main/java/com/atguigu/boot/activemq/MainApp_Produce.java
+++ boot_mq_produce/src/main/java/com/atguigu/boot/activemq/MainApp_Produce.java
@@ -2,10 +2,11 @@ package com.atguigu.boot.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MainApp_Produce {
-
     public static void main(String[] args) {
         SpringApplication.run(MainApp_Produce.class, args);
     }
--- boot_mq_produce/src/main/java/com/atguigu/boot/activemq/produce/Queue_Produce.java
+++ boot_mq_produce/src/main/java/com/atguigu/boot/activemq/produce/Queue_Produce.java
@@ -2,6 +2,7 @@ package com.atguigu.boot.activemq.produce;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
@@ -18,5 +19,12 @@ public class Queue_Produce {
 
     public void produceMsg() {
         jmsMessagingTemplate.convertAndSend(queue, "******: " + UUID.randomUUID().toString().substring(0,6));
+    }
+
+    // 间隔时间3秒钟定投
+    @Scheduled(fixedDelay = 3000)
+    public void produceMsgScheduled() {
+        jmsMessagingTemplate.convertAndSend(queue, "******Scheduled: " + UUID.randomUUID().toString().substring(0,6));
+        System.out.println("******produceMsgSchedule send ok");
     }
 }
