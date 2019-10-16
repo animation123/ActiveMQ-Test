1ca11406e530d4ce8b5b369e30cce832018c0b3c
animation <763678968@qq.com>
2019-10-16 10:01:55 +0800

    NIO传输协议

--- src/main/java/com/atguigu/activemq/queue/JmsConsumer.java
+++ src/main/java/com/atguigu/activemq/queue/JmsConsumer.java
@@ -7,27 +7,19 @@ import java.io.IOException;
 
 public class JmsConsumer {
 
-    //    public static final String ACTIVEMQ_URL = "tcp://192.168.119.128:61616";
-    public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
-    public static final String QUEUE_NAME = "queue01";
+        public static final String ACTIVEMQ_URL = "nio://192.168.119.128:61618";
+//    public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
+    public static final String QUEUE_NAME = "Transport";
 
     public static void main(String[] args) throws JMSException, IOException {
 
-        // 1.创建连接工厂，按照给定的URL地址，采用默认用户名和密码
-        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
 
-        // 2.通过连接工厂，获得连接connection并启动访问
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         Connection connection = activeMQConnectionFactory.createConnection();
         connection.start();
 
-        // 3.创建会话session
-        // 两个参数，第一个为事务/第二个为签收
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-        // 4.创建目的地（具体是队列还是主题）
         Queue queue = session.createQueue(QUEUE_NAME);
-
-        // 5.创建消费者
         MessageConsumer messageConsumer = session.createConsumer(queue);
 
 //        /**
